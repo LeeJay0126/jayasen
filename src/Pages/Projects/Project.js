8cfd2a202322c35e757b47cf59b1b3cd49028998
LeeJay0126 <topaz9889@gmail.com>
2023-12-07 19:17:22 -0800

    .

--- src/Pages/Projects/Project.js
+++ src/Pages/Projects/Project.js
@@ -1,14 +1,20 @@
 import "./Project.css";
 import Menu from "../../SharedComponents/Menu/Menu";
 import Footer from "../../SharedComponents/Footer/Footer";
+import { VscTools } from "react-icons/vsc";
 
 const Project = () => {
 
     return (
         <div className="ProjectPage">
             <Menu />
-            <main>
-
+            <main className="ProjectsMain">
+                <section className="tempUnavail">
+                    <VscTools className="tempImg"/>
+                    <h1 className="ProjectsTempHeading">
+                        COMING SOON
+                    </h1>
+                </section>
             </main>
             <Footer />
         </div>
