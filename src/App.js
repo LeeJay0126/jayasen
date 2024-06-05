98448ae3c768eb31e0cc8b332cce65dd53f7ad3c
LeeJay0126 <topaz9889@gmail.com>
2024-06-05 16:23:24 -0700

    Scroll restoration implemented
    Scroll restoration implemented through useEffect and useLocation.
    
    Component located in SharedComponents folder under "ScrollRestoration.js" file

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import ContactUs from './Pages/ContactUs/ContactUs';
 import Project from './Pages/Projects/Project';
+import ScrollRestoration from './SharedComponents/ScrollRestoration';
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+        <ScrollRestoration />
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/projects" element={<Project/>} />
+          <Route path="/projects" element={<Project />} />
           <Route path="/about" exact element={<About />} />
           <Route path="/contact" element={<ContactUs />} />
         </Routes>
