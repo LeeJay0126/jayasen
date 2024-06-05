5df22e388efc34884c08bbeb104f1452a557dbed
LeeJay0126 <topaz9889@gmail.com>
2024-06-05 16:40:51 -0700

    Update JayProfessional.js
    Updated Jay's professional experience with Midam

--- src/Pages/About/Jay/JayProfessional.js
+++ src/Pages/About/Jay/JayProfessional.js
@@ -1,19 +1,25 @@
 const JayProfessional = [
+    ["Midam", "Frontend Developer", "2023-12", "Present", "Contract",
+        ["Developed and designed a responsive restaurant website using React.js and Figma",
+            "Generated 500+ test cases for the team's feature releases ",
+            "Created QR code that links to restaurant menu to replace the physical menu",
+            "Hosted website application through AWS S3 and Route53 handling over 50,000+ requests"]
+    ],
     ["WBH Ltd", "Web Developer", "2023-08", "2023-12", "Contract, Part Time",
         ["Developed a responsive company website using React, PostgreSQL, Express, and Node.js  for better traffic and inquiry emails",
-        "Designed the layout and workflow of the company's website using Figma",
-        "Managed team progression in a mixed style of Kanban and Scrum"]
+            "Designed the layout and workflow of the company's website using Figma",
+            "Managed team progression in a mixed style of Kanban and Scrum"]
     ],
     ["Pixieset", "SDET", "2022-05", "2022-09", "Intern",
         ["Automated 300+ smoke test cases and 50+ Regression test cases to Pixieset's automation codebase",
-        "Maintained upkeep of features in the company by running weekly smoke testing and regression testing after every feature changes",
-        "Contributed to Studio Manager's end-to-end feature release testing"]
+            "Maintained upkeep of features in the company by running weekly smoke testing and regression testing after every feature changes",
+            "Contributed to Studio Manager's end-to-end feature release testing"]
     ],
     ["Pixieset", "QA Engineer", "2022-01", "2022-05", "Intern",
         ["Ensured team's stable feature release through manual and database testing",
-        "Generated 500+ test cases for the team's feature releases ",
-        "Contributed actively in sprint retrospectives and planning to collaboratively work towards enhanced team culture and practices",
-        "Generated bi-weekly team report for sprint retrospectives to better communicate and forecast team's progression"]
+            "Generated 500+ test cases for the team's feature releases ",
+            "Contributed actively in sprint retrospectives and planning to collaboratively work towards enhanced team culture and practices",
+            "Generated bi-weekly team report for sprint retrospectives to better communicate and forecast team's progression"]
     ]
 ];
 
