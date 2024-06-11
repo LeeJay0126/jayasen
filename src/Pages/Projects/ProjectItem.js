c8b69898cb0bf21008aa11175fd764471d59460a
LeeJay0126 <topaz9889@gmail.com>
2024-06-11 16:03:55 -0700

    Project Card completed
    Need to still work on the responsive ness

--- src/Pages/Projects/ProjectItem.js
+++ src/Pages/Projects/ProjectItem.js
@@ -29,18 +29,20 @@ const ProjectItem = (props) => {
             <div className="projectCircleContainer">
                 {programmingLanguages}
             </div>
-            <section className="projectCardFlex">
-                <h3 className="PCSpan">
-                    Progress: 
-                </h3>
-                {props.progress ? <h3 className="PCStatus PCTrue">In Progress</h3> : <h3 className="PCStatus PCFalse">Completed</h3>}
-            </section>
-            <section className="projectCardFlex">
-                <h3 className="PCSpan">
-                    Deployment: 
-                </h3>
-                {props.deploy ? <h3 className="PCStatus PCTrue">Deployed</h3> : <h3 className="PCStatus PCFalse">Unavailable</h3>}
-            </section>
+            <div className="PCResponsive">
+                <section className="projectCardFlex">
+                    <h3 className="PCSpan">
+                        Progress:
+                    </h3>
+                    {props.progress ? <h3 className="PCStatus PCTrue">In Progress</h3> : <h3 className="PCStatus PCFalse">Completed</h3>}
+                </section>
+                <section className="projectCardFlex">
+                    <h3 className="PCSpan">
+                        Deployment:
+                    </h3>
+                    {props.deploy ? <h3 className="PCStatus PCTrue">Deployed</h3> : <h3 className="PCStatus PCFalse">Unavailable</h3>}
+                </section>
+            </div>
         </div>
     );
 };
