c2db1428a4ab907f801f7297c28822631f74a871
LeeJay0126 <topaz9889@gmail.com>
2024-06-12 17:27:39 -0700

    Fixed Home page latest projects card alignment
    - Home page latest projects' project cards were misaligned when view ports were < 768 and especially when < 600 & mobile.

--- src/Pages/Home/LatestProjectComponent.js
+++ src/Pages/Home/LatestProjectComponent.js
@@ -4,13 +4,14 @@ import ProjectItem from "../Projects/ProjectItem";
 
 const LatestProjectComponent = () => {
     const project1 = ProjectItemList[0];
+    const project2 = ProjectItemList[1];
 
     return (
         <div className="homeLatestProjectsContainer">
-            <div className="homeProjectsFlex">
-                <ProjectItem src={project1[0]} alt={project1[1]} title={project1[2]} lang={project1[3]} 
-                             progress={project1[4]} deploy={project1[5]} url={project1[6]}/>
-            </div>
+            <ProjectItem src={project1[0]} alt={project1[1]} title={project1[2]} lang={project1[3]}
+                progress={project1[4]} deploy={project1[5]} url={project1[6]} />
+            <ProjectItem src={project2[0]} alt={project2[1]} title={project2[2]} lang={project2[3]}
+                progress={project2[4]} deploy={project2[5]} url={project2[6]} />
         </div>
     );
 };
--- src/Pages/Projects/ProjectItemList.js
+++ src/Pages/Projects/ProjectItemList.js
@@ -1,6 +1,7 @@
 
 const ProjectItemList = [
-    ["midam.png", "MidamLogo", "Midam", ["React", "HTML5", "CSS", "Figma"], true, true, "http://www.midam.ca/broadway/menu"]
+    ["midam.png", "MidamLogo", "Midam", ["React", "HTML5", "CSS", "Figma"], true, true, "http://www.midam.ca/broadway/menu"],
+    ["EXpenditure.png", "EXpenditure Logo", "EXpenditure", ["React", "Node", "PosgreSQL", "Express"], false, false,]
 ]
 
 export default ProjectItemList;
