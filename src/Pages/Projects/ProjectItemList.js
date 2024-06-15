4e6fae2a9ade1293654a8c32a5fb483dd3eebbfe
LeeJay0126 <topaz9889@gmail.com>
2024-06-14 19:31:14 -0700

    More project added
    More projects are added in the list for Project section.
    Working on the wireframe of the project page and Projects card
    Project card is scheduled to have a hover on effect to display a button to redirect users to project specific page or a modal.

--- src/Pages/Projects/ProjectItemList.js
+++ src/Pages/Projects/ProjectItemList.js
@@ -1,7 +1,10 @@
 
 const ProjectItemList = [
     ["midam.png", "MidamLogo", "Midam", ["React", "HTML5", "CSS", "Figma"], true, true, "http://www.midam.ca/broadway/menu"],
-    ["EXpenditure.png", "EXpenditure Logo", "EXpenditure", ["React", "Node", "PosgreSQL", "Express"], false, false,]
+    ["EXpenditure.png", "EXpenditure Logo", "EXpenditure", ["React", "Node", "PosgreSQL", "Express"], false, false,],
+    ["LangaraTribe.png", "LangaraTribe Logo", "Langara Tribe", ["React", "Node", "MongoDB", "Express"], false, false,],
+    ["SalaryCalculator.png", "SalaryCalculator Logo", "SalaryCalculator", ["HTML", "CSS", "Javascript"], false, false,],
+    ["Omnifood.png", "Omnifood Logo", "Omnifood", ["HTML", "CSS"], false, false,],
 ]
 
 export default ProjectItemList;
