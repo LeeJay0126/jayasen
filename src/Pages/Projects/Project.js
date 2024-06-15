5346042f2700da5e68fde7c56865380d209e5343
LeeJay0126 <topaz9889@gmail.com>
2024-06-15 14:34:49 -0700

    Project sections added
    Still need to work on the responsiveness of the page.
    Last row of the project cards seem to have odd padding bottom
    Need to add hover-on effect and popup modal for the projects

--- src/Pages/Projects/Project.js
+++ src/Pages/Projects/Project.js
@@ -2,18 +2,28 @@ import "./Project.css";
 import Menu from "../../SharedComponents/Menu/Menu";
 import Footer from "../../SharedComponents/Footer/Footer";
 import { VscTools } from "react-icons/vsc";
+import ProjectItemList from "./ProjectItemList";
+import ProjectItem from "./ProjectItem";
 
 const Project = () => {
+
+    const ProjectComponents = ProjectItemList.map((proj) =>(
+        <ProjectItem src={proj[0]} alt={proj[1]} title={proj[2]} lang={proj[3]}
+        progress={proj[4]} deploy={proj[5]} url={proj[6]} key={proj[2]}/>
+    ));
 
     return (
         <div className="ProjectPage">
             <Menu />
             <main className="ProjectsMain">
-                <section className="tempUnavail">
-                    <VscTools className="tempImg"/>
-                    <h1 className="ProjectsTempHeading">
-                        COMING SOON
+                <section className="sectionIntro">
+                    <VscTools className="ResumeIcon" />
+                    <h1 className="ResumeHeading">
+                        PROJECTS
                     </h1>
+                </section>
+                <section className="projectsContainer">
+                    {ProjectComponents}
                 </section>
             </main>
             <Footer />
