51ca01dfe321f3b8bb8adebe18c017ad0e819076
LeeJay0126 <topaz9889@gmail.com>
2025-02-04 15:24:59 -0800

    Update Midam end date update

--- src/Pages/About/Jay/JayWorkExperience.js
+++ src/Pages/About/Jay/JayWorkExperience.js
@@ -1,6 +1,6 @@
 
 const JayWorkExperience = [
-    ["Midam", "Waiter", "2023-01", "Present", "Part Time",[""]],
+    ["Midam", "Waiter", "2023-01", "2024-11", "Part Time",[""]],
     ["Pita Pit", "Crew Member", "2021-09", "2021-12", "Part Time",[""]],
     ["Kitchen Dada", "Waiter", "2021-01", "2021-06", "Part Time",[""]],
     ["Tapestry Retirement Home", "Waiter", "2020-02", "2020-12", "Part Time",[""]]
