e3c9c16376ceee6f7f7fa719f8de5fc2250fd509
LeeJay0126 <topaz9889@gmail.com>
2025-02-07 10:28:49 -0800

    customer service work experience
    resume updated with more work experiences and details on work duties

--- src/Pages/About/Jay/JayWorkExperience.js
+++ src/Pages/About/Jay/JayWorkExperience.js
@@ -1,9 +1,22 @@
 
 const JayWorkExperience = [
-    ["Midam", "Waiter", "2023-01", "2024-11", "Part Time",[""]],
-    ["Pita Pit", "Crew Member", "2021-09", "2021-12", "Part Time",[""]],
-    ["Kitchen Dada", "Waiter", "2021-01", "2021-06", "Part Time",[""]],
-    ["Tapestry Retirement Home", "Waiter", "2020-02", "2020-12", "Part Time",[""]]
+    ["Midam", "Waiter", "2023-01", "2024-11", "Part Time",
+        ["Communicate special requests, dietary restrictions, or order modifications to the kitchen",
+            "Serve multiple tables at once, ensuring all customers are attended to in a timely manner",
+            "Keep track of frequently used items like menus, order slips, and silverware to avoid shortages during busy periods"
+        ]],
+    ["Hongong Univ", "English Instructor", "2024-06", "2024-08", "Contract",
+        ["Maintain a positive and safe learning environment by managing classroom behavior and resolving conflicts amongstudents",
+            "Provide one-on-one support to students who may need extra help or encouragement with language skills",
+            "Teach grammar rules, new vocabulary, and sentence structures in a clear and engaging way"
+        ]],
+    ["Kitchen Dada", "Waiter", "2021-01", "2021-06", "Part Time", [""]],
+    ["Tapestry Retirement Home", "Waiter", "2020-02", "2020-12", "Part Time",
+        ["Respond to emergency situations, such as falls or health issues, and provide immediate assistance while waiting for medical help",
+            "Engage in conversations, provide emotional support, and help alleviate loneliness among residents, especially during COVID times",
+            "Aid residents in moving around the facility, including helping with walkers, wheelchairs, or transfers from beds to chairs"
+        ]],
+    ["Wireless Wave", "Sales Associate", "2017-02", "2020-01", "Part Time", [""]]
 ];
 
 export default JayWorkExperience;
